8474ec53d5b827a9e6a195df781f04a40678a6b1
haukao <Olaussen99>
2020-03-27 20:05:53 +0100

    changed db

--- angstboksen/settings.py
+++ angstboksen/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'angstboksen.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -101,7 +101,6 @@ DATABASES = {
     }
 }
 
-'''
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
